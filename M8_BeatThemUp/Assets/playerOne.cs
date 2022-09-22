172463bd4b72c1402e641856afbea16139fefea0
Hysteriallisione <yangtchenheitz@yahoo.com.au>
2022-09-22 16:04:17 +0200

    script player1 animations

--- M8_BeatThemUp/Assets/playerOne.cs
+++ M8_BeatThemUp/Assets/playerOne.cs
@@ -11,7 +11,8 @@ public class playerOne : MonoBehaviour
     public Animator oneAnim;
     public float Horizontal;
     public float Vertical;
-    
+    public bool Jumping;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,23 +24,57 @@ public class playerOne : MonoBehaviour
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
-        Vertical = Input.GetAxisRaw("Vertical");
-        Vector2 movePlayerOne = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        vecPlayer = movePlayerOne.normalized * speed;
-        oneAnim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        Vertical = Input.GetAxis("Vertical");  
+        vecPlayer.x = rigidB.velocity.x;
+        vecPlayer.y = rigidB.velocity.y;
+        oneAnim.SetBool("playerMoving", true);
+        Jumping = false;
 
-        oneAnim.SetBool("Attack", false);
+        if(vecPlayer.magnitude > 0)
+        {
+            oneAnim.SetBool("playerMoving", true);
+        }else
+        {
+            oneAnim.SetBool("playerMoving", false);
+        }
 
-        if (Input.GetKey(KeyCode.JoystickButton1))
+        if(Input.GetKey(KeyCode.Joystick1Button0))
         {
             oneAnim.SetBool("Attack", true);
         }
-        if (Input.GetKeyUp(KeyCode.JoystickButton1))
+        if(Input.GetKeyUp(KeyCode.Joystick1Button0))
         {
             oneAnim.SetBool("Attack", false);
         }
-      
-    }
+       /* if( Horizontal < 0)
+        {
+            this.transform.localScale = new Vector3(-4, 4, 1);
+            oneAnim.SetBool("playerMoving", true);
+        }
+        if (Horizontal > 0)
+        {
+          
+            this.transform.localScale = new Vector3(4, 4, 1);
+            oneAnim.SetBool("playerMoving", true);
+        }*/
+        if (Input.GetKey(KeyCode.JoystickButton1))
+        {
+            Jumping = true;
+            oneAnim.SetBool("jumping", true);
+        }
+        if(Input.GetKeyUp(KeyCode.JoystickButton1))
+        {
+            Jumping = false;
+            oneAnim.SetBool("jumping", false);
+        }
 
+
+    }
+    private void FixedUpdate()
+    {
+        rigidB.velocity = new Vector2(Horizontal, Vertical) * speed;
+     
+        
+    }
 }
 
